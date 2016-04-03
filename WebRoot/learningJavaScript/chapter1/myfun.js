0f5bca118bc9fdaf5a0a3f9e4623fb24b3069709
YouShareRose <941375265@qq.com>
2016-04-03 14:34:33 +0800

    Learning JavaScript chapter1 hello,world

---
+++ WebRoot/learningJavaScript/chapter1/myfun.js
@@ -0,0 +1,12 @@
+/*
+ * 函数名：hello
+ * 作者： Candy
+ * 输出 hello , world
+ */
+function hello() {
+	//say hello
+	var msg = "Hello, <em>world!</em>";
+	document.open();
+	document.write(msg);
+	document.close();
+}
